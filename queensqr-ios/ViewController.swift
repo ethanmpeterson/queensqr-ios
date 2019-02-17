24c691600c7942888964a54bb76c3112881f7a0d
Ethan Peterson <epeterson@rsgc.on.ca>
2019-02-17 13:54:36 -0500

    Initial Commit

---
+++ queensqr-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  queensqr-ios
+//
+//  Created by Ethan Peterson on 2019-02-17.
+//  Copyright © 2019 Ethan Peterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
