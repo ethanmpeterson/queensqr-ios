23a1d2917915ca2ebdbd9e7293a329cbe6b07f2c
Ethan Peterson <epeterson@rsgc.on.ca>
2019-02-18 17:18:19 -0500

    Service view done

--- queensqr-ios/ServiceViewController.swift
+++ queensqr-ios/ServiceViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class ServiceViewController: UIViewController {
     
+    @IBOutlet weak var roomNumber: UILabel!
+    @IBOutlet weak var facultyLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var monday: UILabel!
+    @IBOutlet weak var tuesday: UILabel!
+    @IBOutlet weak var wednesday: UILabel!
+    @IBOutlet weak var thursday: UILabel!
+    @IBOutlet weak var friday: UILabel!
+    @IBOutlet weak var saturday: UILabel!
+    @IBOutlet weak var sunday: UILabel!
+    
+    
     var service : [String : Any]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(service)
-        // Do any additional setup after loading the view.
+        
+        // Filter Data and Update Views and Labels
+        self.navigationItem.title = "\(service["name"] as! String)"
+        roomNumber.text = "Room Number: \(service["room_number"] as! String)"
+        facultyLabel.text = "Faculty: \(service["faculty"] as! String)"
+        descriptionLabel.text = "Description: \(service["description"] as! String)"
+        
+        // Update Hours
+        let hoursDict = service["hours"] as! [String : String]
+        monday.text = "Monday: \(hoursDict["mon"] as! String)"
+        tuesday.text = "Tuesday: \(hoursDict["tue"] as! String)"
+        wednesday.text = "Wednesday: \(hoursDict["wed"] as! String)"
+        thursday.text = "Thursday: \(hoursDict["thu"] as! String)"
+        friday.text = "Friday: \(hoursDict["fri"] as! String)"
+        saturday.text = "Saturday: \(hoursDict["sat"] as! String)"
+        sunday.text = "Sunday: \(hoursDict["sun"] as! String)"
     }
     
 
