b0929d9d37f1b9522fc19e0af72b29d2840b0012
Ethan Peterson <epeterson@rsgc.on.ca>
2019-02-18 00:40:26 -0500

    added table cell view for presenting buildings with help of youtube tutorial

--- queensqr-ios/BuildingViewController.swift
+++ queensqr-ios/BuildingViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 import Alamofire
 
+struct cellData {
+    var opened = Bool()
+    var title = String()
+    var sectionData = [String]()
+}
+
 class BuildingViewController: UITableViewController {
     
     
@@ -18,9 +24,17 @@ class BuildingViewController: UITableViewController {
     
     var URL : String = ""
     var building : [String : Any]?
+    
+    var tableViewData = [cellData]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableViewData = [
+            cellData(opened: false, title: "Alias", sectionData: ["1", "2", "3"]),
+            cellData(opened: false, title: "faculty", sectionData: ["1", "2", "3"]),
+            cellData(opened: false, title: "Description", sectionData: ["1", "2", "3"])
+        ]
         
         print(qrData)
         print(apiRoot!)
@@ -54,8 +68,48 @@ class BuildingViewController: UITableViewController {
         }
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return tableViewData.count
+    }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableViewData[section].opened {
+            return tableViewData[section].sectionData.count + 1
+        } else {
+            return 1
+        }
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var dataIndex = indexPath.row - 1
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                return UITableViewCell()
+            }
+            cell.textLabel?.text = tableViewData[indexPath.section].title
+            return cell
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+                return UITableViewCell()
+            }
+            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[dataIndex]
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            if tableViewData[indexPath.section].opened {
+                tableViewData[indexPath.section].opened = false
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            } else {
+                tableViewData[indexPath.section].opened = true
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            }
+        }
+    }
     
     /*
     // MARK: - Navigation
